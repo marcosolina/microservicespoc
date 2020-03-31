62aa8dc68028e482961c407540bbda385eceb52d
msolina <msolina@ixico.com>
2020-03-31 17:59:24 +0200

    experiments

---
+++ marco-security-service/src/main/java/com/marco/securityservice/MarcoSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marco.securityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcoSecurityServiceApplication.class, args);
+	}
+
+}
