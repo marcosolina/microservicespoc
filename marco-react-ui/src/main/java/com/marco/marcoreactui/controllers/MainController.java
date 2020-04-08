f131f171f0984d1eeb2d1fd51c7e3105a17a8834
marco <marco@localhost>
2020-04-09 00:00:00 +0200

    UI first commit

---
+++ marco-react-ui/src/main/java/com/marco/marcoreactui/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.marco.marcoreactui.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping(value = "/")
+	public String mainController() {
+		return "index";
+	}
+}
