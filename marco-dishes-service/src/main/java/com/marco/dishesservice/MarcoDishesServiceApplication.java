3981369a25923633140d4b788c5b741756e1f633
marco <marco@localhost>
2020-04-11 13:08:01 +0200

    reverting few things

--- marco-dishes-service/src/main/java/com/marco/dishesservice/MarcoDishesServiceApplication.java
+++ marco-dishes-service/src/main/java/com/marco/dishesservice/MarcoDishesServiceApplication.java
@@ -2,10 +2,8 @@ package com.marco.dishesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication()
 public class MarcoDishesServiceApplication {
 
 	public static void main(String[] args) {
