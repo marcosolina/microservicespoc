e8b72cb06baf144dd18d549d46c91cbe21feab8c
marco <marco@192.168.1.8>
2020-04-13 11:03:11 +0200

    updating menu service

---
+++ marco-menu-service/src/main/java/com/marco/menuservice/utils/PricesConstants.java
@@ -0,0 +1,10 @@
+package com.marco.menuservice.utils;
+
+public class PricesConstants {
+
+	private PricesConstants() {
+		
+	}
+	
+	public static final String TOKEN_DISHES_REGISTRATION_ID = "dishes-service";
+}
