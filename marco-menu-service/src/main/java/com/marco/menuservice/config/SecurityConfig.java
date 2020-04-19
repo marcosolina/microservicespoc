47be862783fc0335840cb8a2c43004c6271eb2c6
marco <marco@192.168.1.8>
2020-04-19 13:46:51 +0200

    menu UI

--- marco-menu-service/src/main/java/com/marco/menuservice/config/SecurityConfig.java
+++ marco-menu-service/src/main/java/com/marco/menuservice/config/SecurityConfig.java
@@ -16,9 +16,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests()
-			.anyRequest().permitAll();
 		/*
 		 * Standard spring security
 		 */
